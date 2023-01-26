fe382a56404616ed7d33cb7a1639b744c58896ab
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-26 23:32:05 +0530

    Finished

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
   console.log('App is now ready');
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  console.log(event);
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('Video duration is: ', metadata.format.duration);
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
